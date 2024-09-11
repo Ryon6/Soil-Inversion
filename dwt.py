c6b15ea15b82b19899b5bf7497547e0759cb09ae
Ryon6 <61035808+ryon6@users.noreply.github.com>
2024-09-11 19:58:31 +0800

    efficient discrete wavelet transform(dwt.py)
    Signed-off-by: 3470494694 <3470494694@qq.com>

--- dwt.py
+++ dwt.py
@@ -1,4 +1,3 @@
-# TODO: 提高效率
 import numpy as np
 import pywt
 from load_data import load_mining_region_data
@@ -8,7 +7,7 @@ import matplotlib.pyplot as plt
 
 def wavelet_denoising(img, wavelet='db4', level=3):
     """
-    使用小波变换对高光谱图像进行降噪
+    使用小波变换对高光谱图像进行降噪（批量处理）
 
     Args:
         img: 高光谱图像数据，形状为(波段数, 高, 宽)
@@ -19,30 +18,30 @@ def wavelet_denoising(img, wavelet='db4', level=3):
         降噪后的高光谱图像
     """
 
-    # 获取图像尺寸
-    bands, height, width = img.shape
+    if img.ndim == 3:
+        # 获取图像尺寸
+        bands, height, width = img.shape
+        # Reshape图像，将每个像素点的光谱作为一个向量
+        signals = img.reshape(bands, -1)
+    elif img.ndim == 2:
+        signals = img
 
-    # 初始化降噪后的图像
-    denoised_img = np.zeros_like(img)
+    # 对所有像素点的光谱进行小波变换
+    coeffs = pywt.wavedec(signals, wavelet, level=level, axis=0)
 
-    # 对每个像素点对应的光谱进行降噪
-    for i in range(height):
-        for j in range(width):
-            # 提取当前像素点的光谱曲线
-            coeffs = pywt.wavedec(img[:, i, j], wavelet, level=level)
+    # 设置阈值
+    threshold = np.median(np.abs(coeffs[-level])) / 0.6745
 
-            # 设置阈值（这里使用软阈值，你可以根据实际情况调整）
-            threshold = np.median(np.abs(coeffs[-level])) / 0.6745
+    # 对细节系数进行阈值处理
+    for k in range(1, len(coeffs)):
+        coeffs[k] = pywt.threshold(coeffs[k], threshold, mode='soft')
 
-            # 对细节系数进行阈值处理
-            for k in range(1, len(coeffs)):
-                coeffs[k] = pywt.threshold(coeffs[k], threshold, mode='soft')
+    # 重构信号
+    denoised_img = pywt.waverec(coeffs, wavelet, axis=0)
 
-            # 重构信号
-            denoised_signal = pywt.waverec(coeffs, wavelet)
-
-            # 将降噪后的信号保存到输出图像中
-            denoised_img[:, i, j] = denoised_signal
+    # Reshape回原来的形状
+    if img.ndim == 3:
+        denoised_img = denoised_img.reshape(bands, height, width)
 
     return denoised_img
 
@@ -59,32 +58,17 @@ def main():
     samples_spectral = first_order_differential(samples_spectral, wavelengths, axis=1)
     img_array = first_order_differential(img_array, wavelengths, axis=0)
 
-    reconstructed_image = wavelet_denoising(img_array, wavelet='db4', level=4)
+    # reconstructed_image = wavelet_denoising(img_array, wavelet='db4', level=4)
+    reconstructed_samples_spectral = wavelet_denoising(samples_spectral.T, wavelet='db4', level=4)
 
     # 提取原始和重构的光谱曲线
-    original_spectra = img_array[:, 1, 1]
-    reconstructed_spectra = reconstructed_image[:, 1, 1]
+    # original_spectra = img_array[:, 1, 1]
+    # reconstructed_spectra = reconstructed_image[:, 1, 1]
+    original_spectra = samples_spectral.T[:, 0]
+    reconstructed_spectra = reconstructed_samples_spectral[:, 0]
     plt.plot(original_spectra)
     plt.plot(reconstructed_spectra)
     plt.show()
-
-    # # 绘制光谱曲线图
-    # plt.figure(figsize=(12, 6))
-    # plt.subplot(1, 2, 1)
-    # plt.plot(original_spectra)
-    # # plt.imshow(original_spectra, cmap='gray')
-    # plt.title('Original Spectra')
-    # plt.axis('off')
-    #
-    # plt.subplot(1, 2, 2)
-    # plt.plot(reconstructed_spectra)
-    # # plt.imshow(reconstructed_spectra, cmap='gray')
-    # plt.title('Reconstructed Spectra')
-    # plt.axis('off')
-    #
-    # plt.tight_layout()
-    # plt.show()
-
 
 
 if __name__ == '__main__':
